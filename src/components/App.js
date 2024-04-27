d1f23f295c7181dc6de55b88caa3a52c781984f7
lewis karanu <lewiskaranu0@gmail.com>
2024-04-27 23:11:56 +0300

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,15 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About(){
+  return(
+    <div id="about">
+      <h2>About</h2>
+
+      </div>
+
+  )
+}
 
 
 function App() {
@@ -24,7 +33,11 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home/>
+
       {/* add your <About> component here */}
+      <About/>
+
     </div>
   );
 }
